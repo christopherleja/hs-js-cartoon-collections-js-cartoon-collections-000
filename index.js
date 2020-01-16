c14177863174aeb72ce68f15d0f5338b24ad05ef
Christopher Leja <christopher.leja@gmail.com>
2020-01-16 19:00:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(dwarves) { 
+var listDwarves = []
+  for(let i=0; i<dwarves.length; i++){
+    listDwarves.push(`${i+1}. ${dwarves[i]} `)
+}
+  return listDwarves.join("");
 }
 
 function summonCaptainPlanet(planeteerCalls){
-}
+  var uppercasePlaneteerCalls = [];
+    for (let i=0; i<planeteerCalls.length; i++){
+      var emphasis = "!"
+      uppercasePlaneteerCalls.push(`${planeteerCalls[i]}${emphasis}`.toUpperCase())
+    }
+  return uppercasePlaneteerCalls
+  }
+
 
 function longPlaneteerCalls(words) {
-}
+  var assortedWords = [];
+  for (let i=0; i<words.length; i++){
+    if (words[i].length > 4)
+    return true
+  }
+    return false
+} 
 
 function findTheCheese (foods) {
+  for (let i=0; i<foods.length; i++) {
+  
+  if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert") {
+  return foods[i];
+  } 
+} 
+return "no cheese!";
 }
+
