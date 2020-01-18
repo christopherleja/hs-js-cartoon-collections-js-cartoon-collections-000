abba2e91ede60273208bab0b21c38a9cfb7c2eab
Christopher Leja <christopher.leja@gmail.com>
2020-01-18 18:37:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ return "no cheese!";
 function wordsWithB (words) {
   var wordsThatStartWithB = [];
   for (let i=0; i,words.length; i++){
-  if (words[i][0] === 'b')  {  
+  if (words[i][0] === 'b' || words[i][0] === "B")  {  
     wordsThatStartWithB.push(words[i])}
 }
 return wordsThatStartWithB
